c29d6db2faae79dc9afef1b16f58f790d66e9fa1
WeslleyKatsube <161661648+WeslleyKatsube@users.noreply.github.com>
2024-02-28 22:43:43 -0300

    aula 28-02
    aula 28-02

---
+++ src/ex_2/index.js
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function Mensagem () {
+    return(
+        <View>
+            <Text>Hello Reacrt-Native</Text>
+        </View>
+    );
+}
